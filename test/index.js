2048a321849b19d9f74188c6c110c4da74ea326e
dsshim <daveshim@gmail.com>
2015-10-21 23:03:37 -0600

    fixes tests and separates responsibility

--- test/index.js
+++ test/index.js
@@ -68,11 +68,11 @@ describe('Canvas boundries', function () {
     var block = new Block(0, 0, 10, 10, Block.canvas, Block.context);
 
     block.move("left")
-    assert.equal(block.x, 400)
+    assert.equal(block.x, 600)
   });
 
   it('moves off the screen from right to left', function () {
-    var block = new Block(400, 0, 10, 10, Block.canvas, Block.context);
+    var block = new Block(600, 0, 10, 10, Block.canvas, Block.context);
 
     block.move("right")
     assert.equal(block.x, 0)
@@ -82,7 +82,7 @@ describe('Canvas boundries', function () {
     var block = new Block(0, 0, 10, 10, Block.canvas, Block.context);
 
     block.move("up")
-    assert.equal(block.y, 600)
+    assert.equal(block.y, 400)
   });
 
   it('moves off the screen from bottom to top', function () {
@@ -98,7 +98,7 @@ describe('Canvas boundries', function () {
       var block = new Block(0, 0, 10, 10, Block.canvas, Block.context);
       block.move('right');
       block.move('left');
-       
+
     });
   });
 });
